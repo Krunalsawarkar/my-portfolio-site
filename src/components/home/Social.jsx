d930b8066a5344c6c06b1e8b342887c2ef8e8c55
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-06-19 13:32:48 +0530

    Initial Commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+        <a href="https://www.linkedin.com/in/krunal-sawarkar-a4a98a181/" className="home__social-icon" target='_blank'>
+        <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://github.com/Krunalsawarkar" className="home__social-icon" target='_blank'>
+        <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href="https://twitter.com/Iamkrunal68" className="home__social-icon" target='_blank'>
+        <i class="uil uil-twitter-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
